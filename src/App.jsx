ef2f9dd7044d8ff2786c9e0dd91c2aa870857457
valerDev <valer.val7@gmail.com>
2023-09-30 01:39:16 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import s from './App.module.scss';
+import {Home} from './pages/Home/Home'
+function App() {
+  return (
+    <div className={s.App}>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import s from './Home.module.scss';
+
+export const Home = () => {
+    return <div className={s.home}>
+        <div className={s.homeBackground}></div>
+    </div>
+}
