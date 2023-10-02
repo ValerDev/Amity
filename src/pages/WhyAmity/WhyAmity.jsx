6f395d38c1517a539cca3a1710e746afdbf1cabe
valerDev <valer.val7@gmail.com>
2023-10-02 18:50:41 +0400

    changes

--- src/pages/WhyAmity/WhyAmity.jsx
+++ src/pages/WhyAmity/WhyAmity.jsx
@@ -9,20 +9,22 @@ export const WhyAmity = () => {
             <h4 className={s.subTitle}>AMITY</h4>
             <p>Our team consists of experienced logistic specialists and lovely support representatives who are ready to wipe up your daily stress and fill your day with peace of mind.</p>
         </div>
-        <div className={s.mainContent}>
-            <h3 className={s.title}>SERVICES</h3>
-            <div className={s.services}>
-                <div className={s.serviceBlock}>
-                    <h4 className={s.title}>REGULAR</h4>
-                    <p className={s.description}>Regular service type offers affordable solutions for any of your transportation needs based on your flexibility. </p>
-                </div>
-                <div className={s.serviceBlock}>
-                    <h4 className={s.title}>TIME-LIMITED</h4>
-                    <p className={s.description}>Are you in a rush and terribly need something done? No worries! Our friendly team will make sure to take well care of your shipment and get everything set according your plans.</p>
-                </div>
-                <div className={s.serviceBlock}>
-                    <h4 className={s.title}>ASSISTED</h4>
-                    <p className={s.description}>Do you need help to figure out how to get something on and off the truck? Do you need to store the commodity but don't know where to find room for it? Need a helping hand that exactly knows how to do it? Our experienced market specialists will arrange and take care of all logistical difficulties that you are facing.</p>
+        <div className={s.servicesContent}>
+            <div className={s.mainContent}>
+                <h3 className={s.title}>SERVICES</h3>
+                <div className={s.services}>
+                    <div className={s.serviceBlock}>
+                        <h4 className={s.title}>REGULAR</h4>
+                        <p className={s.description}>Regular service type offers affordable solutions for any of your transportation needs based on your flexibility. </p>
+                    </div>
+                    <div className={s.serviceBlock}>
+                        <h4 className={s.title}>TIME-LIMITED</h4>
+                        <p className={s.description}>Are you in a rush and terribly need something done? No worries! Our friendly team will make sure to take well care of your shipment and get everything set according your plans.</p>
+                    </div>
+                    <div className={s.serviceBlock}>
+                        <h4 className={s.title}>ASSISTED</h4>
+                        <p className={s.description}>Do you need help to figure out how to get something on and off the truck? Do you need to store the commodity but don't know where to find room for it? Need a helping hand that exactly knows how to do it? Our experienced market specialists will arrange and take care of all logistical difficulties that you are facing.</p>
+                    </div>
                 </div>
             </div>
             <div className={`${s.serviceBlock} ${s.communication}`}>
@@ -30,6 +32,6 @@ export const WhyAmity = () => {
                 <p className={s.description}>Here at Amity Transport we evaluate the communication and are ready to enlighten all questions arising about the transportation. We do think that communication is the key and will be in touch during the whole shipping process.</p>
             </div>
         </div>
-            <img src={whyAmityBackground} alt="whyAmityBackground" />
+        <img src={whyAmityBackground} alt="whyAmityBackground" />
     </div>
 }
